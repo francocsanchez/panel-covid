81d5e947e0dfc94b9910945ff1f2597f24542e89
Franco Sanchez <francocsanchez@gmail.com>
2020-05-11 16:43:47 -0300

    create layouts app

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('img/logo.png') }}" class="logo-brand" alt="logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <ion-icon name="menu-outline"></ion-icon>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href=""></a>
+                </li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+            	<li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        DATA
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{route('list.efectores')}}">Efectores</a>
+                        <a class="dropdown-item" href="{{route('list.estados-clinicos')}}">Estados Clinicos</a>
+                        <a class="dropdown-item" href="{{route('list.financiador')}}">Financiador</a>
+                        <a class="dropdown-item" href="{{route('list.fuentes')}}">Fuentes</a>
+                        <a class="dropdown-item" href="{{route('list.motivos')}}">Motivos</a>
+                        <a class="dropdown-item" href="{{route('list.pacientes')}}">Pacientes</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
