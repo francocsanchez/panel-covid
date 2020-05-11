1c964258682a3ed13c6792f0e4d8e985e2b59e18
Franco Sanchez <francocsanchez@gmail.com>
2020-05-11 20:10:34 -0300

    create panel.blade.php

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -1,1 +1,36 @@
-index.blade.php
+@extends('layouts.app')
+
+@section('content')
+<section class="panel-container">
+	<div class="container">
+		<h1 class="display-4">RESUMEN DE DATOS</h1>
+		<div class="row">
+			<div class="col">
+				<div class="card border-info">
+					<div class="card-header text-white bg-info text-center">TOTAL</div>
+					<div class="card-body">
+						<p class="card-text">Registros ingresados: </p>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card border-success">
+					<div class="card-header text-white bg-success text-center">COMPLETOS</div>
+					<div class="card-body">
+						<p class="card-text">Registros completos: </p>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card border-danger">
+					<div class="card-header text-white bg-danger text-center">INCOMPLETOS</div>
+					<div class="card-body">
+						<p class="card-text">Registros descartados: </p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr>
+	</div>
+</section>
+@endsection
