fd96909625471c8e9640b89220b754cbfe5bb8be
Franco Sanchez <francocsanchez@gmail.com>
2020-05-29 21:56:38 -0300

    Integreation CSS - Panel Covid RN

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,103 @@
+<div class="sidebar-wrapper sidebar-theme">
+	<nav id="compactSidebar">
+		<ul class="menu-categories">
+			<li class="menu active">
+				<a href="partessalud.html" data-active="true" class="menu-toggle">
+					<div class="base-menu">
+						<div class="base-icons">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text">
+								<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+								<polyline points="14 2 14 8 20 8"></polyline>
+								<line x1="16" y1="13" x2="8" y2="13"></line>
+								<line x1="16" y1="17" x2="8" y2="17"></line>
+								<polyline points="10 9 9 9 8 9"></polyline>
+							</svg>
+						</div>
+						<span>
+							Partes de<br>
+							Prensa
+						</span>
+					</div>
+				</a>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left">
+					<polyline points="15 18 9 12 15 6"></polyline>
+				</svg>
+			</li>
+			<li class="menu">
+				<a href="partessalud.html" data-active="false" class="menu-toggle">
+					<div class="base-menu">
+						<div class="base-icons">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text">
+								<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+								<polyline points="14 2 14 8 20 8"></polyline>
+								<line x1="16" y1="13" x2="8" y2="13"></line>
+								<line x1="16" y1="17" x2="8" y2="17"></line>
+								<polyline points="10 9 9 9 8 9"></polyline>
+							</svg>
+						</div>
+						<span>
+							Partes de<br>
+							Salud
+						</span>
+					</div>
+				</a>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left">
+					<polyline points="15 18 9 12 15 6"></polyline>
+				</svg>
+			</li>
+			<li class="menu">
+				<a href="seguimientocuarentena.html" data-active="false" class="menu-toggle">
+					<div class="base-menu">
+						<div class="base-icons">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
+								<rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+								<line x1="16" y1="2" x2="16" y2="6"></line>
+								<line x1="8" y1="2" x2="8" y2="6"></line>
+								<line x1="3" y1="10" x2="21" y2="10"></line>
+							</svg>
+						</div>
+						<span>
+							Seguimiento de <br>
+							Cuarentena
+						</span>
+					</div>
+				</a>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left">
+					<polyline points="15 18 9 12 15 6"></polyline>
+				</svg>
+			</li>
+			<li class="menu">
+				<a href="internacionesyplazas.html" data-active="false" class="menu-toggle">
+					<div class="base-menu">
+						<div class="base-icons">
+							<img src="{{ asset('test/assets/img/icons/hospitales.svg') }}" class="navbar-logo" alt="logo" style="width:45px; height:45px;">
+						</div>
+						<span>Internaciones<br>y Plazas</span>
+					</div>
+				</a>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline>
+				</svg>
+			</li>
+			<li class="menu">
+				<a href="numerosglobales.html" data-active="false" class="menu-toggle">
+					<div class="base-menu">
+						<div class="base-icons">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe">
+								<circle cx="12" cy="12" r="10"></circle>
+								<line x1="2" y1="12" x2="22" y2="12"></line>
+								<path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+							</svg>
+						</div>
+						<span>
+							Números <br>
+							Globales
+						</span>
+					</div>
+				</a>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left">
+					<polyline points="15 18 9 12 15 6"></polyline>
+				</svg>
+			</li>
+		</ul>
+	</nav>
+</div>
